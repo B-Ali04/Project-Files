55b340bc6bde6e8de11af8b196763d844dee6a43
B-Ali04 <86619743+B-Ali04@users.noreply.github.com>
2022-01-28 11:12:18 -0500

    Update NIH-NSF_2.sql

--- NIH-NSF_2.sql
+++ NIH-NSF_2.sql
@@ -1,14 +1,36 @@
 Select
     GORADID.GORADID_ADDITIONAL_ID SU_ID,
     --unit_name, unit_id, cip_code, person_id
-
+    (select 0 from dual) unit_name,
+    (select -1 from dual) unit_id,
+        
+    SPRIDEN.SPRIDEN_ID person_id,
+    STVMAJR.STVMAJR_CIPC_CODE cip_code,
     SPBPERS.SPBPERS_ETHN_CDE citizen_race_ethnicity_code,
-SGBSTDN.SGBSTDN_DEGC_CODE_1 degree_code,
-SRVYSTU.SRVYSTU_FULL_PART_IND enrollment_code,
-SPBPERS.SPBPERS_SEX sex_code,
-SGBSTDN.SGBSTDN_STYP_CODE firsttime_code
-    
+
+    CASE
+        when SGBSTDN.SGBSTDN_DEGC_CODE_1 = 'PHD' then 1
+        else 2
+    END degree_code,
+
+    CASE
+        when SRVYSTU.SRVYSTU_FULL_PART_IND = 'F' then 1
+        else 2
+    END enrollment_code,
+
+    CASE
+        when SPBPERS.SPBPERS_SEX = 'M' then 1
+        when SPBPERS.SPBPERS_SEX = 'F' then 2
+        else null
+    END sex_code,
+
+    CASE
+        when SGBSTDN.SGBSTDN_STYP_CODE = 'N' then 1
+        else 2
+    END firsttime_code,    
 --   support_source_code, support_mechanism_code
+    (select 1 from dual) support_source_code,
+    (select 2 from dual) support_mechanism_code
 
 from
     SPRIDEN SPRIDEN
@@ -35,6 +57,11 @@ from
          and srvystu_posterm_class_yr_code not in ('BG')
          and srvystu_type_code not in ('X')
          and srvystu_curric_1_major_code not in ('UNDC','SUS','VIS','0000')
+    
+    join STVMAJR STVMAJR on STVMAJR.STVMAJR_CODE = SGBSTDN.SGBSTDN_MAJR_CODE_1
+    
+    join STVCIPC STVCIPC on STVCIPC.STVCIPC_CODE = STVMAJR.STVMAJR_CIPC_CODE
+             
 where
     SPRIDEN.SPRIDEN_NTYP_CODE is null
     and SPRIDEN.SPRIDEN_CHANGE_IND is null
@@ -46,4 +73,4 @@ where
         and SFRSTCR.SFRSTCR_RSTS_CODE in ('RE','RW'))
 
 order by
-    sgbstdn.sgbstdn_degc_code_1 desc, spbpers.spbpers_sex desc, stvstyp_surrogate_id asc
+    SGBSTDN.SGBSTDN_DEGC_CODE_1 desc, SPBPERS.SPBPERS_SEX desc, STVSTYP.STVSTYP_SURROGATE_ID asc
