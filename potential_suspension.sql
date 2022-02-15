150a764a462dafda1eb1ed31063dcd6a1a89b752
B-Ali04 <86619743+B-Ali04@users.noreply.github.com>
2022-02-15 09:41:04 -0500

    Create potential_suspension.sql

---
+++ potential_suspension.sql
@@ -0,0 +1,102 @@
+
+Select
+    (select 1 from dual) Continuing_Probation,
+    SPRIDEN.SPRIDEN_ID Banner_ID,
+    f_format_name(SPRIDEN.SPRIDEN_PIDM, 'LF30') Student_Name,
+    stvdegc.stvdegc_desc deg_code,
+    stvclas_code class_code,
+        trunc(SHRTGPA.SHRTGPA_GPA, 3) Semester_GPA,
+        trunc(SHRLGPA.SHRLGPA_GPA, 3) Cumulative_GPA,    
+    REL.ASTD_CODE, rel.*
+
+from
+    SPRIDEN SPRIDEN
+
+    join STVTERM STVTERM on STVTERM.STVTERM_CODE = 202240 --:parm_term_code_select.STVTERM_CODE
+
+    join GORADID GORADID on GORADID.GORADID_PIDM = SPRIDEN.SPRIDEN_PIDM
+         and GORADID.GORADID_ADID_CODE = 'SUID'
+
+    left outer join GOREMAL GOREMAL on GOREMAL.GOREMAL_PIDM = SPRIDEN.SPRIDEN_PIDM
+        and GOREMAL.GOREMAL_EMAL_CODE = 'SU'
+        and GOREMAL.GOREMAL_STATUS_IND = 'A'
+
+    join SGBSTDN SGBSTDN on SGBSTDN.SGBSTDN_PIDM = SPRIDEN.SPRIDEN_PIDM
+         and SGBSTDN.SGBSTDN_TERM_CODE_EFF = fy_sgbstdn_eff_term(SGBSTDN.SGBSTDN_PIDM, STVTERM.STVTERM_CODE)
+         and SGBSTDN.SGBSTDN_MAJR_CODE_1 not in ('0000', 'EHS', 'SUS', 'VIS', 'UNDC')
+         and SGBSTDN.SGBSTDN_STST_CODE = 'AS'
+
+    left outer join REL_STUDENT_STANDING REL on REL.PIDM = SPRIDEN.SPRIDEN_PIDM
+         and REL.TERM_CODE = STVTERM.STVTERM_CODE
+         and REL.ASTD_TERM_CODE <= STVTERM.STVTERM_CODE
+
+    left outer join SHRTGPA SHRTGPA on SHRTGPA.SHRTGPA_PIDM = SPRIDEN.SPRIDEN_PIDM
+        and SHRTGPA.SHRTGPA_TERM_CODE = REL.TERM_CODE
+        and SHRTGPA.SHRTGPA_GPA_TYPE_IND = 'I'
+
+    left outer join SHRLGPA SHRLGPA on SHRLGPA.SHRLGPA_PIDM = SPRIDEN.SPRIDEN_PIDM
+         and SHRLGPA.SHRLGPA_LEVL_CODE = SGBSTDN.SGBSTDN_LEVL_CODE
+         and SHRLGPA.SHRLGPA_GPA_TYPE_IND = 'I'
+         and SHRLGPA.SHRLGPA_GPA is not null
+         
+    join STVDEGC STVDEGC on STVDEGC.STVDEGC_CODE = SGBSTDN.SGBSTDN_DEGC_CODE_1
+
+    join STVMAJR STVMAJR on STVMAJR.STVMAJR_CODE = SGBSTDN.SGBSTDN_MAJR_CODE_1
+    
+    join STVSTYP STVSTYP on STVSTYP.STVSTYP_CODE = SGBSTDN.SGBSTDN_STYP_CODE
+    
+    join STVCLAS STVCLAS on STVCLAS.STVCLAS_CODE = f_class_calc_fnc(SGBSTDN.SGBSTDN_PIDM,SGBSTDN.SGBSTDN_LEVL_CODE, rel.TERM_CODE)
+
+
+where
+    SPRIDEN.SPRIDEN_NTYP_CODE is null
+    and SPRIDEN.SPRIDEN_CHANGE_IND is null
+    and exists(
+        select * from SFRSTCR SFRSTCR
+        where SFRSTCR.SFRSTCR_PIDM = SPRIDEN.SPRIDEN_PIDM
+              and SFRSTCR.SFRSTCR_TERM_CODE = STVTERM.STVTERM_CODE
+              and SFRSTCR.SFRSTCR_RSTS_CODE in ('RE','RW')
+    )
+    
+    and SGBSTDN.SGBSTDN_LEVL_CODE = 'UG'
+    and REL.ASTD_CODE = 'P1' 
+
+and(
+    (
+        SHRLGPA.SHRLGPA_GPA_HOURS < 30
+        and SHRLGPA.SHRLGPA_GPA < 1.700
+    )
+    or(
+        SHRLGPA.SHRLGPA_GPA_HOURS > 30
+        and SHRLGPA.SHRLGPA_GPA_HOURS <= 60
+        and SHRLGPA.SHRLGPA_GPA < 1.850
+    )
+    or(
+        SHRLGPA.SHRLGPA_GPA_HOURS > 60
+        and SHRLGPA.SHRLGPA_GPA < 2.000
+    )
+)
+    /*
+    and(
+    
+    )   
+   
+    and ((
+         SHRLGPA.SHRLGPA_HOURS_EARNED < 30.0
+         and SHRLGPA.SHRLGPA_GPA < 1.7000000
+        )
+        or(
+        SHRLGPA.SHRLGPA_HOURS_EARNED >= 30.0
+        and SHRLGPA.SHRLGPA_HOURS_EARNED < 60.0
+        and SHRLGPA.SHRLGPA_GPA < 1.850
+        )
+        or(
+        SHRLGPA.SHRLGPA_HOURS_EARNED >= 60.0
+        and SHRLGPA.SHRLGPA_GPA < 2.000
+        )
+    )
+    */
+order by
+
+  --STVCLAS.STVCLAS_SURROGATE_ID asc, stvstyp.stvstyp_surrogate_id desc, 
+  SPRIDEN.SPRIDEN_LAST_NAME asc, rel.term_code--, SPRIDEN.SPRIDEN_FIRST_NAME
